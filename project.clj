ffc972690be6005c856904f592b360874ef47f7d
Hoppy <hoppy@gnurdle.com>
2016-11-01 19:35:54 -0500

    in goes the goo

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject com.gnurdle/ace2-fonts "0.1.0"
+  :description "FIXME: write description"
+  :url "http://gnurdle.com"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :source-paths [])
